bafd58ee29f3e54776ee3b62f9fb82f198d093e2
Kamlesh Das <daskamlesh677@gmail.com>
2024-08-18 14:29:21 +0530

    Testing file added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Multi-level-Comment-System
+
+## User Authentication Apis:
+    
+   * user registration api: localhost:4560/api/user/register
+   * user login api : localhost:4560/api/user/register
+      
---
+++ index.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const app=express();
+require("dotenv").config();
+const{connection}=require("./config/db");
+const {userRouter}=require("./Routes/user.router");
+const {postRouter}=require("./Routes/post.router");
+const{commentRouter}=require("./Routes/comment.router")
+const cookieParser=require("cookie-parser");
+const{authenticate}=require("./Middlewares/authentication");
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.send("welcome to the Multylevel Comment system");
+})
+
+app.use("/api/user",userRouter);
+app.use(authenticate);
+app.get('/api/auth',(req,res)=>{
+    res.send("this is the routes that will open after authentication");
+})
+app.use("/api",postRouter);
+app.use("/api/posts",commentRouter);
+
+
+
+
+
+const PORT=process.env.PORT || 4500;
+app.listen(PORT,async()=>{
+    try {
+        await connection;
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(`app is running at port {PORT}...`)
+})
